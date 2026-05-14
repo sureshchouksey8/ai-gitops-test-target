a6eabe07c73e7624781af9be378d3174a5135514
sureshchouksey8 <sureshchouksey8@gmail.com>
2026-05-14 19:58:36 +0530

    Use shared task file validation

--- commands/list.py
+++ commands/list.py
@@ -1,21 +1,8 @@
 """List tasks command."""
 
 import json
-from pathlib import Path
 
-
-def get_tasks_file():
-    """Get path to tasks file."""
-    return Path.home() / ".local" / "share" / "task-cli" / "tasks.json"
-
-
-def validate_task_file():
-    """Validate tasks file exists."""
-    # NOTE: Validation logic scattered here - should be in utils (refactor bounty)
-    tasks_file = get_tasks_file()
-    if not tasks_file.exists():
-        return []
-    return tasks_file
+from utils.validation import validate_task_file
 
 
 def list_tasks():
